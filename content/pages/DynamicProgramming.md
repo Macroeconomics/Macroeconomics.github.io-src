6b05bf971f692ae9ba47e7d1da54f5d6ba18368b
Ömer Özak <ozak@users.noreply.github.com>
2017-01-04 13:31:30 -0600

    V1.5

--- content/pages/DynamicProgramming.md
+++ content/pages/DynamicProgramming.md
@@ -5,3 +5,4 @@ date: 2016-12-29 13:53
 Tags: Dynamic Programming in IPython
 Author: Ömer Özak
 
+{% notebook notebooks/DynamicProgramming.ipynb %}
---
+++ workscript.sh
@@ -0,0 +1,44 @@
+
+# Go to directory
+cd ~/GitHub/Macroeconomics.github.io-src
+
+# Clone
+git clone git@github.com:Macroeconomics/Macroeconomics.github.io-src ghpages
+
+# Move into directory for pages and check
+cd ghpages
+git remote -v
+
+# Clone output
+git submodule add git@github.com:Macroeconomics/Macroeconomics.github.io.git output
+
+# Create baseline
+pelican-quickstart
+
+# Create content and see website
+make html && make serve
+
+# If it is ok
+make publish
+
+# Publish and git commit
+cd output
+git add .
+git commit -m "V.1.0"
+git push -u origin master
+cd ..
+echo '*.pyc' >> .gitignore #don't need pyc files
+git add .
+git commit -m "V1.0"
+git push -u origin master
+
+# Publish and git commit
+cd output
+git add .
+git commit -m "V.1.5"
+git push -u origin master
+cd ..
+git add .
+git commit -m "V1.5"
+git push -u origin master
+
