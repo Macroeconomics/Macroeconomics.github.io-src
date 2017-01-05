7d0e33ff4b58e66b8f79b36fd735a651ad296d5f
Ömer Özak <ozak@users.noreply.github.com>
2017-01-04 18:28:28 -0600

    Overwrote output 2

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 import os
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 # Me
 AUTHOR = u'Ömer Özak'
 AUTHORS = u'Ömer Özak'
--- workscript.sh
+++ workscript.sh
@@ -35,10 +35,10 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "Overwrote output"
+git commit -m "Overwrote output 2"
 git push -u origin master
 cd ..
 git add .
-git commit -m "Overwrote output"
+git commit -m "Overwrote output 2"
 git push -u origin master
 
