43472ba5d86c29dd9d3155cb07c26a8e3a802fb5
Ömer Özak <ozak@users.noreply.github.com>
2017-02-08 11:57:49 -0600

    wdi

--- workscript.sh
+++ workscript.sh
@@ -36,10 +36,10 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "computation"
+git commit -m "wdi"
 git push -u origin master
 cd ..
 git add .
-git commit -m "computation"
+git commit -m "wdi"
 git push -u origin master
 
