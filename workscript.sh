791cfc890b598b7601768783b01a90e48ba9aea6
Ömer Özak <ozak@users.noreply.github.com>
2017-02-10 12:22:24 -0600

    pwt

--- workscript.sh
+++ workscript.sh
@@ -36,10 +36,10 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "wdi"
+git commit -m "pwt"
 git push -u origin master
 cd ..
 git add .
-git commit -m "wdi"
+git commit -m "pwt"
 git push -u origin master
 
