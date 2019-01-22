3e92935e547dea804c37da4bb2113a7f99339da2
Ömer Özak <ozak@users.noreply.github.com>
2019-01-22 10:20:50 -0600

    Updated syllabus again

--- workscript.sh
+++ workscript.sh
@@ -37,9 +37,9 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "Updated syllabus"
+git commit -m "Updated syllabus again"
 git push -u origin master
 cd ..
 git add .
-git commit -m "Updated  syllabus"
+git commit -m "Updated syllabus again"
 git push -u origin master
