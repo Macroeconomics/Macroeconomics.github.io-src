027c2a95bf23ce0117d4355bdf9deb577c678453
Ömer Özak <ozak@users.noreply.github.com>
2019-01-22 10:17:21 -0600

    Updated  syllabus

--- workscript.sh
+++ workscript.sh
@@ -37,9 +37,9 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "Updated Python 3"
+git commit -m "Updated syllabus"
 git push -u origin master
 cd ..
 git add .
-git commit -m "Updated Python 3"
+git commit -m "Updated  syllabus"
 git push -u origin master
