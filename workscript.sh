8f8d43baf35c507dff392749ab89637c41c24319
Ömer Özak <ozak@users.noreply.github.com>
2020-03-03 09:38:06 -0600

    Updated Lecture 5

--- workscript.sh
+++ workscript.sh
@@ -41,9 +41,9 @@ git push -u origin master
 # Publish and git commit
 cd output
 git add .
-git commit -m "Updated Lecture 4 & 5"
+git commit -m "Updated Lecture 5"
 git push -u origin master
 cd ..
 git add .
-git commit -m "Updated Lecture 4 & 5"
+git commit -m "Updated Lecture 5"
 git push -u origin master
